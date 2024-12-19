d3e90146262fb0a2e9916019742ace2c47d9ebc2
Ariz992 <163850743+Ariz992@users.noreply.github.com>
2024-12-19 11:53:07 -0300

    cartel data
    raycast e interaccion terminado, cartel data finalizado y funcional.

---
+++ Assets/CartelData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CartelData : MonoBehaviour
+{
+    public string mensaje;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
--- Assets/raycastInteraction.cs
+++ Assets/raycastInteraction.cs
@@ -1,16 +1,19 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class raycastInteraction : MonoBehaviour
 {
     public float RayLenght;
     public Transform RayOrigin;
     public LayerMask LayerMask;
+    public GameObject hint;
+    public CartelData cartel;
     // Start is called before the first frame update
     void Start()
     {
-        
+        hint.SetActive(false);
     }
 
     // Update is called once per frame
@@ -20,7 +23,15 @@ public class raycastInteraction : MonoBehaviour
         if (Physics.Raycast(RayOrigin.position, RayOrigin.forward, out hit, RayLenght, LayerMask))
         {
             Debug.Log("hit con " + hit.collider.name);
+            cartel = hit.collider.GetComponent<CartelData>();
+            hint.SetActive(true);
+            hint.GetComponent<Text>().text = cartel.mensaje;
         }
+        else
+        {
+            hint.SetActive(false);
+        }
+
     }
     private void OnDrawGizmos()
     {
